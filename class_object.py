1f171b4c38b6ceae840cd35a8ad17c3faf8a8bc3
Deepachourasia <deepach2004@gmail.com>
2026-05-26 11:19:20 +0530

    Add files via upload

---
+++ class_object.py
@@ -0,0 +1,68 @@
+# class CarTemplate:  #blueprint
+#     #Constructor are the function which is used to define or create the object 
+#     #constructor are the function python me _init_ 
+#     #which are used to create the object the object or initialize 
+#     #object are car maruti ,nexon ,
+#     def CarTemplate
+#     color:"red"   #class
+#     price=19000
+#     seat=4
+
+# maruti=CarTemplate()
+
+# alto=CarTemplate()
+# print("alto:",alto.color,alto.price,CarTemplate.color)
+
+#self ak variable h jo us constructor ko call kr raha hai
+#self nhi denge toh kese pta chlega ki kiske liye kaam ho raha hai
+# class CarTemplate:   #blueprint
+      
+#       def _init_(self,gadi_color ):   #def _init_ it is constructor
+#         print("constructor runn ::===>",self)  # jo bhi kaaam krvana h vo isme kravyge
+#         self.color=gadi_color #local variable  
+        
+# # print(CarTemplate())
+# maruti=CarTemplate("yellow")   #maruti ak variable jo is car template object ka memory store kr raha h
+# print(maruti.color)
+# # print("maruti :: ===>",maruti.color)
+
+
+
+# tarzen=CarTemplate("purple")
+# print("Tarzen  ===>",tarzen.color)
+
+
+# class student:
+    
+#     school='st.pauls'  #class variable
+#     def _init_(self,stu_name,stu_id,stu_email,stu_school_name,stu_fees):
+#          self.id=stu_id
+#          self.name=stu_name
+#          self.email=stu_email
+#          self.fees=stu_fees
+#          self.school=stu_school_name
+
+#     student1=student('123','deepa','deepach20042gmail.com','')
+
+# class Student:
+    
+#     school = 'st.pauls'   # class variable
+
+#     def __init__(self, stu_id, stu_name, stu_email, stu_school_name, stu_fees):
+#         self.id = stu_id
+#         self.name = stu_name
+#         self.email = stu_email
+#         self.fees = stu_fees
+        
+#         # object variable (optional override)
+#         if stu_school_name:
+#             self.school = stu_school_name
+
+
+# # create object
+# student1 = Student('123', 'Deepa', 'deepach20042@gmail.com', '', 50000)
+
+# # access data
+# print(student1.id, student1.name, student1.email, student1.school, student1.fees)
+
+        
