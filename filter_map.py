baae7700ae5d4b37614661612f5e9aefd6c6fefd
Deepachourasia <deepach2004@gmail.com>
2026-05-30 21:41:48 +0530

    Add files via upload

---
+++ filter_map.py
@@ -0,0 +1,12 @@
+# out=list(map(lambda a: a%2==0 ,[10,13,15,24]))
+# print("out with map :",out)
+
+# out=list(filter(lambda a: a%2==0,[10,13,15,24]))
+# print("out with map:",out)
+
+# out=filter(lambda a: a%2==0,[10,13,15,24])
+# print("out with map:",out)
+# print(next(out))
+# print(next(out))
+
+
