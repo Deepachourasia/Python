4f6b1a9752573f8be6cb1fc3de2d775c660d4a74
Deepachourasia <deepach2004@gmail.com>
2026-05-30 21:44:17 +0530

    Add files via upload

---
+++ whilestring.py
@@ -0,0 +1,53 @@
+# s='RAJU'
+# i=0
+# while(i<len(s)):
+#     print("INdex",i,"character:",s[i])
+#     i+=1
+
+# # last character print krvane ke liye 
+# s='RAJU'
+# i=0
+# j=len(s)-1
+# while(i<len(s)):
+#     print("INdex",i,"character:",s[i],"LastIndex:",j)
+#     i+=1
+#     j-=1
+
+#  #  iski approach issi chiz ko 2pointer bolte h jo aage or piche 2 variables ko use kkrke apna sara kaam krte haai
+# s='NISHA'
+# i=0
+# j=len(s)-1
+# while(i<len(s)):
+#     print("INdex",i,"character:",s[i],"LastIndex:",s[j])
+#     i+=1
+#     j-=1
+
+
+# without slincing do pallindrom .what is slicing?slicing is like data[ : :-1]but we dont have to do from this
+
+# s='SATRS'
+# i=0
+# j=len(s)-1
+# z=0 
+# while(i<len(s)):
+#     print("INdex",i,"character:s[i]","LastIndex:",j,"LastCharacter:",s[j])
+# if(s[i] == s[j]):
+#     z=1
+#      break
+
+#     i+=1
+#     j-=1
+
+# print(z)
+
+
+# s='SAMST'
+# i=0
+# j=len(s)-1
+# z=0
+# while(i<j):
+#     print("INDex:","character:",s[i],"Last Index:",j,"Last Character:"s[j])
+#     if(s[i]==s[j]):
+#         z=1
+        
+#         print
