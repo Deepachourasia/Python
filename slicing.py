96a7c0fba2c5485a232a31630615eef71733e17a
Deepachourasia <deepach2004@gmail.com>
2026-05-30 21:46:00 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,15 @@
+state="Rajasthan"
+# print(state[0]) #index 
+
+# state="Rajasthan"
+# print(state[0:4]) #slicing (0 to 3) 
+
+# state="Rajasthan"
+# print(state[1:6]) #slicing (1 to 5) 
+
+# state="Rajasthan"
+# print(state[1:6]) #slicing (1 to 5) 
+
+# print("Negative:",state[-1:-4:-1])
+
+print("Negative:",state[-1:-5:-1])
