3921c6f00047a776fb5238e99365bcbf162e7a11
Deepachourasia <deepach2004@gmail.com>
2026-05-30 21:46:29 +0530

    Add files via upload

---
+++ membership_op.py
@@ -0,0 +1,77 @@
+"""print("hey")
+a=50
+if(a>200 or 7>a):
+    print("condition sahi hai")#indentation
+else:
+    print("ab hai condition galat")
+
+print("Deepa")"""
+
+"""a = 24
+
+if a % 2 == 0 and a % 5 == 0 and a % 7 == 0:
+    print("No is divisible by 2, 5 and 7")
+
+elif a % 2 == 0 and a % 5 == 0:
+    print("No is divisible by 2 and 5")
+
+elif a % 2 == 0:
+    print("No is divisible by 2")
+
+else:
+    print("No is not divisible by 2, 5, or 7")"""
+
+
+"""a=28
+if a % 3 == 0 and a % 7 == 0 :
+    print("No. is divisible by 3 and 7")
+elif a % 3 == 0 and a % 7 == 0  and a % 2 == 0: 
+    print("No. is divisible by  3 ,7 and it is even")
+elif a % 3 == 0 and a % 7 == 0  and a!= 0:
+    print("No. is divisible by 3 and 7  and it is odd")
+else :
+    print("no. is not divisible by 3 and 7")"""
+
+"""a = 28
+
+if a % 3 == 0 and a % 7 == 0:
+
+    if a % 2 == 0:
+        print("No. is divisible by 3 and 7 and it is even")
+    else:
+        print("No. is divisible by 3 and 7 and it is odd")
+
+else:
+    print("No. is not divisible by 3 and 7")"""
+
+"""k = 10
+
+if k % 5 == 0 and k % 8 == 0:
+    if k % 2 == 0:
+        print("no. is divisible by 5 and 8 and it is even")
+        print("hello user")
+    else:
+        print("Deepa")
+else:
+    print("Condition failed")"""
+
+"""year = int(input ("Enter year :"))
+
+if year % 400 == 0:
+    print("leap year")
+elif year % 4 == 0 and year % 100 != 0:
+    print("leap year")
+else:
+    print("not a leap year")"""
+    
+
+
+
+
+
+
+
+
+
+
+
