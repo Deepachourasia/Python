20293e19adfb5a19314e5e135f678dc8d217d5ec
Deepachourasia <deepach2004@gmail.com>
2026-05-30 21:40:39 +0530

    Add files via upload

---
+++ dictonary.py
@@ -0,0 +1,73 @@
+#self
+
+# info={
+#     "key":"value",
+#     "name":"apnacollege",
+#     "learning":"coding",
+#     "name":"Deepa",
+#     "subjects":["python","C","Java"],
+#     "topics":("dict","set"),
+#     "age":35,
+#     "is_adult":True,
+#     "marks":94.4,
+#     12.99:94.4
+
+# }
+# print(info)
+# print(type(info))
+# info["name"]="Deepa" #overwrite                                                                                                                                                                                                                                                                                                      
+# info["surname"]="chourasia"
+# print(info)
+
+# null_dict={}
+# null_dict["name"]="apnacollege"
+# print(null_dict)
+
+# Nexted dictionary(dictonary ke andr 1 or dictonary ko add krna)
+#  kisi bhi key ki value ko apneapp me ak dictonary bana skte  hai
+# student = {
+#     "name":"rahul kumar",
+
+#     "subjects":{
+#         "phy":97,
+#         "chem":98,
+#         "math":95
+#     }
+# }
+
+# print(student["subjects"]["chem"])
+# print(pairs[0])
+
+# pairs = list(student.items())
+# print(pairs[0])
+
+# method == myDict.get("key") #returns the key according to value 
+# print(student["name"])
+# print(student.get("name"))
+# if in case we written
+# print(student["name2"]) #error
+# print(student.get("name2")) #no error ->None
+
+# print(student["name2"]) #error
+# print("hi")
+# print("welcome to")
+# print("apnacollege")
+# print("we are learning")
+# print("coding")
+
+# myDict.update(newDict) # inserts the specified items to the dictonary
+
+# student = {
+#     "name":"rahul kumar",
+
+#     "subjects":{
+#         "phy":97,
+#         "chem":98,
+#         "math":95
+#     }
+# }
+
+# new_dict = {"name":"deepa ","age":16}
+# student.update(new_dict)
+
+# print(student)
